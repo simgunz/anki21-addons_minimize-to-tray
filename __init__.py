a6fa0f33ba6a2896e4dd70f39adf9241303fa036
Simone Gaiarin <simgunz@gmail.com>
2020-02-13 11:09:05 +0100

    Show all windows when clicking the tray icon
    Previously I was hiding them, but it makes more sense to show the windows when they are minimized for example.

--- __init__.py
+++ __init__.py
@@ -8,52 +8,69 @@
 # Report any problem in the github issues section
 
 from types import MethodType
+from typing import NamedTuple
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QApplication, QMenu, QSystemTrayIcon
+from PyQt5.QtWidgets import QWidget, QApplication, QMenu, QSystemTrayIcon
 
 from anki import hooks
 from aqt import mw              #mw is the INSTANCE of the main window (aqt/main.py) create in aqt/__init__
 from aqt.main import AnkiQt
 
+
+class Window(NamedTuple):
+    obj: QWidget
+    state: Qt.WindowState
+
+
 def onFocusChanged(self, old, now):
     """Keep track of the focused window in order to refocus it on showAll
     """
-    if now == None:
-        self.last_focus = old
+    if now is not None:
+        self.last_focus = now
+
+def showWindows(self, windows):
+    for w in windows:
+        if w.obj.isWindow():
+            w.obj.show()
+            w.obj.setWindowState(w.state)
 
 def showAll(self):
     """Show all windows
     """
-    for w in self.tray_hidden:
-        if w.isWindow() and w.isHidden():
-            w.showNormal()
-    active = self.last_focus
-    active.raise_()
-    active.activateWindow()
+    if self.anki_visible:
+        self.showWindows(self.visibleWindows())
+    else:
+        self.showWindows(self.tray_hidden)
+    self.last_focus.activateWindow()
     self.anki_visible = True
-    self.tray_hidden = []
 
 def hideAll(self):
     """Hide all windows
     """
     self.tray_hidden = []
+    windows = self.visibleWindows()
+    for w in windows:
+        w.obj.hide()
+    self.tray_hidden = windows
+    self.anki_visible = False
+
+def visibleWindows(self):
+    windows = []
     for w in QApplication.topLevelWidgets():
         if w.isWindow() and not w.isHidden():
             if not w.children():
                 continue
-            w.hide()
-            self.tray_hidden.append(w)
-    self.anki_visible = False
+            statefulWindow = Window(w, w.windowState())
+            windows.append(statefulWindow)
+    return windows
 
 def trayActivated(self, reason):
     """Show/hide all Anki windows when the tray icon is clicked
     """
     if reason == QSystemTrayIcon.Trigger:
-        if self.anki_visible:
-            self.hideAll()
-        else:
-            self.showAll()
+        self.showAll()
 
 def createSysTray(self):
     """Create an system tray with the Anki icon
@@ -75,38 +92,40 @@ def createSysTray(self):
     self.trayIcon.activated.connect(self.trayActivated)
     self.app.focusChanged.connect(self.onFocusChanged)
     self.trayIcon.show()
-            
+
 def minimizeToTrayClose(self):
     self.closeEventFromAction = True
     self.close()
-        
+
 def wrapCloseCloseEvent():
     "Override an existing method of an instnce of an object"
     def repl(self, event):
         if not self.closeEventFromAction:
             #self.col.save()
-            self.trayActivated(QSystemTrayIcon.Trigger)
-            event.ignore();
+            self.hideAll()
+            event.ignore()
             return
         AnkiQt.closeEvent(self, event)
     return MethodType(repl, mw)
-    
+
 def minimizeToTrayInit():
     if hasattr(mw, 'trayIcon'):
         return
     mw.closeEventFromAction = False
     mw.createSysTray()
     # Disconnecting from close may have some side effects (e.g. QApplication::lastWindowClosed() signal not emitted)
-    mw.form.actionExit.triggered.disconnect(mw.close)                  
+    mw.form.actionExit.triggered.disconnect(mw.close)
     mw.form.actionExit.triggered.connect(mw.minimizeToTrayClose)
     config = mw.addonManager.getConfig(__name__) # Get addon config
     if config['hide_on_startup']:
         mw.hideAll()
 
 # Set Anki main window new methods
-mw.onFocusChanged = MethodType(onFocusChanged, mw) 
-mw.showAll = MethodType(showAll, mw) 
-mw.hideAll = MethodType(hideAll, mw) 
+mw.onFocusChanged = MethodType(onFocusChanged, mw)
+mw.showWindows = MethodType(showWindows, mw)
+mw.showAll = MethodType(showAll, mw)
+mw.hideAll = MethodType(hideAll, mw)
+mw.visibleWindows = MethodType(visibleWindows, mw)
 mw.trayActivated = MethodType(trayActivated, mw)
 mw.createSysTray = MethodType(createSysTray, mw)
 
