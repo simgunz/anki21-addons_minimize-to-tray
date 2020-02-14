f08ef5980bb582f05a41fa05026ebf0a1b0c672d
Simone Gaiarin <simgunz@gmail.com>
2020-02-14 09:09:19 +0100

    Refactoring: create AnkiSystemTray class
    In this way we avoid binding too many methods to mw with possible conflicts

--- __init__.py
+++ __init__.py
@@ -24,112 +24,114 @@ class Window(NamedTuple):
     state: Qt.WindowState
 
 
-def onFocusChanged(self, old, now):
-    """Keep track of the focused window in order to refocus it on showAll
-    """
-    if now is not None:
-        self.last_focus = now
-
-def showWindows(self, windows):
-    for w in windows:
-        if w.obj.isWindow():
-            w.obj.show()
-            w.obj.setWindowState(w.state)
-
-def showAll(self):
-    """Show all windows
-    """
-    if self.anki_visible:
-        self.showWindows(self.visibleWindows())
-    else:
-        self.showWindows(self.tray_hidden)
-    self.last_focus.activateWindow()
-    self.anki_visible = True
-
-def hideAll(self):
-    """Hide all windows
-    """
-    self.tray_hidden = []
-    windows = self.visibleWindows()
-    for w in windows:
-        w.obj.hide()
-    self.tray_hidden = windows
-    self.anki_visible = False
-
-def visibleWindows(self):
-    windows = []
-    for w in QApplication.topLevelWidgets():
-        if w.isWindow() and not w.isHidden():
-            if not w.children():
-                continue
-            statefulWindow = Window(w, w.windowState())
-            windows.append(statefulWindow)
-    return windows
-
-def trayActivated(self, reason):
-    """Show/hide all Anki windows when the tray icon is clicked
-    """
-    if reason == QSystemTrayIcon.Trigger:
-        self.showAll()
-
-def createSysTray(self):
-    """Create an system tray with the Anki icon
-    """
-    # Check if self (i.e., mw.aqt) already has a trayIcon
-    if hasattr(self, 'trayIcon'):
-        return
-    self.anki_visible = True
-    self.last_focus = self
-    self.trayIcon = QSystemTrayIcon(self)
-    ankiLogo = QIcon()
-    ankiLogo.addPixmap(QPixmap(":/icons/anki.png"), QIcon.Normal, QIcon.Off)
-    self.trayIcon.setIcon(QIcon.fromTheme("anki", ankiLogo))
-    trayMenu = QMenu(self)
-    self.trayIcon.setContextMenu(trayMenu)
-    showAction = trayMenu.addAction("Show all windows")
-    showAction.triggered.connect(self.showAll)
-    trayMenu.addAction(self.form.actionExit)
-    self.trayIcon.activated.connect(self.trayActivated)
-    self.app.focusChanged.connect(self.onFocusChanged)
-    self.trayIcon.show()
-
-def minimizeToTrayClose(self):
-    self.closeEventFromAction = True
-    self.close()
-
-def wrapCloseCloseEvent():
-    "Override an existing method of an instnce of an object"
-    def repl(self, event):
-        if not self.closeEventFromAction:
-            #self.col.save()
+class AnkiSystemTray():
+    def __init__(self, mw):
+        """Create a system tray with the Anki icon."""
+        self.mw = mw
+        self.anki_visible = True
+        self.last_focus = mw
+        self.trayIcon = self._createTrayIcon()
+        self._configureMw()
+        self.trayIcon.show()
+        config = self.mw.addonManager.getConfig(__name__)
+        if config['hide_on_startup']:
             self.hideAll()
-            event.ignore()
-            return
-        AnkiQt.closeEvent(self, event)
-    return MethodType(repl, mw)
+
+    def _configureMw(self):
+        self.mw.closeEventFromAction = False
+        self.mw.app.focusChanged.connect(self.onFocusChanged)
+        # Disconnecting from close may have some side effects
+        # (e.g. QApplication::lastWindowClosed() signal not emitted)
+        self.mw.form.actionExit.triggered.disconnect(self.mw.close)
+        self.mw.form.actionExit.triggered.connect(self.onClose)
+        self.mw.closeEvent = self._wrapCloseCloseEvent()
+
+    def trayActivated(self, reason):
+        """Show/hide all Anki windows when the tray icon is clicked
+        """
+        if reason == QSystemTrayIcon.Trigger:
+            self.showAll()
+
+    def onFocusChanged(self, old, now):
+        """Keep track of the focused window in order to refocus it on showAll
+        """
+        if now is not None:
+            self.last_focus = now
+
+    def onClose(self):
+        self.mw.closeEventFromAction = True
+        self.mw.close()
+
+    def showAll(self):
+        """Show all windows
+        """
+        if self.anki_visible:
+            self._showWindows(self._visibleWindows())
+        else:
+            self._showWindows(self.tray_hidden)
+        self.last_focus.activateWindow()
+        self.last_focus.raise_()
+        self.anki_visible = True
+
+    def hideAll(self):
+        """Hide all windows
+        """
+        self.tray_hidden = []
+        windows = self._visibleWindows()
+        for w in windows:
+            w.obj.hide()
+        self.tray_hidden = windows
+        self.anki_visible = False
+
+    def _showWindows(self, windows):
+        for w in windows:
+            if w.obj.isWindow():
+                if w.state == Qt.WindowMinimized:
+                    w.obj.showNormal()
+                else:
+                    w.obj.show()
+                    w.obj.setWindowState(w.state)
+                w.obj.raise_()
+
+    def _visibleWindows(self):
+        windows = []
+        for w in QApplication.topLevelWidgets():
+            if w.isWindow() and not w.isHidden():
+                if not w.children():
+                    continue
+                statefulWindow = Window(w, w.windowState())
+                windows.append(statefulWindow)
+        return windows
+
+    def _createTrayIcon(self):
+        trayIcon = QSystemTrayIcon(self.mw)
+        ankiLogo = QIcon()
+        ankiLogo.addPixmap(QPixmap(":/icons/anki.png"), QIcon.Normal, QIcon.Off)
+        trayIcon.setIcon(QIcon.fromTheme("anki", ankiLogo))
+        trayMenu = QMenu(self.mw)
+        trayIcon.setContextMenu(trayMenu)
+        showAction = trayMenu.addAction("Show all windows")
+        showAction.triggered.connect(self.showAll)
+        trayMenu.addAction(self.mw.form.actionExit)
+        trayIcon.activated.connect(self.trayActivated)
+        return trayIcon
+
+    def _wrapCloseCloseEvent(self):
+        "Override an existing method of an instance of an object"
+        def repl(self, event):
+            if not self.closeEventFromAction:
+                #self.col.save()
+                self.systemTray.hideAll()
+                event.ignore()
+                return
+            AnkiQt.closeEvent(self, event)
+        return MethodType(repl, self.mw)
+
 
 def minimizeToTrayInit():
     if hasattr(mw, 'trayIcon'):
         return
-    mw.closeEventFromAction = False
-    mw.createSysTray()
-    # Disconnecting from close may have some side effects (e.g. QApplication::lastWindowClosed() signal not emitted)
-    mw.form.actionExit.triggered.disconnect(mw.close)
-    mw.form.actionExit.triggered.connect(mw.minimizeToTrayClose)
-    config = mw.addonManager.getConfig(__name__) # Get addon config
-    if config['hide_on_startup']:
-        mw.hideAll()
+    mw.systemTray = AnkiSystemTray(mw)
 
-# Set Anki main window new methods
-mw.onFocusChanged = MethodType(onFocusChanged, mw)
-mw.showWindows = MethodType(showWindows, mw)
-mw.showAll = MethodType(showAll, mw)
-mw.hideAll = MethodType(hideAll, mw)
-mw.visibleWindows = MethodType(visibleWindows, mw)
-mw.trayActivated = MethodType(trayActivated, mw)
-mw.createSysTray = MethodType(createSysTray, mw)
-
-mw.minimizeToTrayClose = MethodType(minimizeToTrayClose, mw)
-mw.closeEvent = wrapCloseCloseEvent()
 
 hooks.addHook("profileLoaded", minimizeToTrayInit)
